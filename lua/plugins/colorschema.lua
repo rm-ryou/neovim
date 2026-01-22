72ab2cb6e2870cea321fa1f78db63c20cdbad947
rm-ryou <rmco.420118@gmail.com>
2026-01-22 13:28:05 +0900

    init

---
+++ lua/plugins/colorschema.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("tokyonight").setup({
+      transparent = true,
+      style = "night",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_colors = function(colors)
+        colors.bg_statusline = colors.none
+      end,
+    })
+
+    vim.cmd([[colorscheme tokyonight]])
+  end,
+}
