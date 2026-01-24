e5ceb50fb5aa7be7040b3f4721cb4b8144a66008
rm-ryou <rmco.420118@gmail.com>
2026-01-24 13:47:24 +0900

    feat: impl lualine settings

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,42 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = function()
-    -- TODO
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      -- set an empty statusline till lualine loads
+      vim.o.statusline = " "
+    else
+      -- hide the statusline on the starter page
+      vim.o.laststatus = 0
+    end
   end,
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = "auto",
+      globalstatus = true,
+      disabled_filetypes = {
+        statusline = { "snacks_dashboard", "snacks_picker_list" },
+        winbar = { "snacks_dashboard", "snacks_picker_list" },
+      },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch" },
+      lualine_c = { "filename" },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    extensions = { "neo-tree", "lazy", "fzf" },
+  },
 }
